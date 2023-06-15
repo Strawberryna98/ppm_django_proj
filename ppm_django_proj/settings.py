3363bbf942b1189dbe5e23446fc0d083080c00d2
Strawberryina <giulia.lagreca14@gmail.com>
2023-06-15 16:22:32 +0200

    modified gitignore

--- ppm_django_proj/settings.py
+++ ppm_django_proj/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +29,6 @@ MEDIA_URL = '/media/'
 # SECURITY WARNING: keep the secret key used in production secret!
 #print(f"MY_SECRET_KEY: {os.getenv('MY_SECRET_KEY')}")
 SECRET_KEY = os.getenv('MY_SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
